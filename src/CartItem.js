ffd40b731b6a6896b9f896fd0845d23f502b141c
Unmesh Pawar <unplfc@gmail.com>
2023-02-20 21:06:26 +0530

    set state in depth

--- src/CartItem.js
+++ src/CartItem.js
@@ -24,9 +24,26 @@ class CartItem extends React.Component {
       return {
         qty: prevState.qty + 1
       }
+    }, () => {
+      console.log('this.state', this.state);
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
+    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
     });
   }
   render () {
+    console.log('render');
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
@@ -42,13 +59,14 @@ class CartItem extends React.Component {
             <img
               alt="increase"
               className="action-icons"
-              src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+              src="https://image.flaticon.com/icons/svg/992/992651.svg"
               onClick={this.increaseQuantity}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
