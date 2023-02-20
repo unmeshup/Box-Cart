5d7304a45c278aa49ac819874b4d92727b060ba4
Unmesh Pawar <unplfc@gmail.com>
2023-02-21 00:09:55 +0530

    Props

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,36 +1,24 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-   //  this.testing();
-  }
+  // testing () {
+  //   const promise = new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve('done');
+  //     }, 5000);
+  //   })
 
- //  testing () {
- //    const promise = new Promise((resolve, reject) => {
-     //  setTimeout(() => {
-     //   resolve('done');
-     // }, 5000);
-    //})
+  //   promise.then(() => {
+  //     // setState acts like a synchronus call
+  //     this.setState({ qty: this.state.qty + 10 });
 
-    // promise.then(() => {
-    
-     // this.setState({ qty: this.state.qty + 10 });
+  //     this.setState({ qty: this.state.qty + 10 });
 
-    // this.setState({ qty: this.state.qty + 10 });
+  //     this.setState({ qty: this.state.qty + 10 });
 
-    //  this.setState({ qty: this.state.qty + 10 });
-
-    //  console.log('state', this.state);
-    //});
-   //}
+  //     console.log('state', this.state);
+  //   });
+  // }
   increaseQuantity = () => {
     // this.state.qty += 1;
     // console.log('this', this.state);
@@ -61,10 +49,11 @@ class CartItem extends React.Component {
     });
   }
   render () {
-    console.log('render');
-    const { price, title, qty } = this.state;
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
     return (
       <div className="cart-item">
+        {this.props.jsx}
         <div className="left-block">
           <img style={styles.image} />
         </div>
