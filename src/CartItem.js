0d33a1bd0c777f7dd4dc9d5d41e4a1e1a57ac8bd
Unmesh Pawar <unplfc@gmail.com>
2023-02-24 21:16:32 +0530

    decrease and delete

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,6 +4,12 @@ class CartItem extends React.Component {
   render () {
     console.log('this.props', this.props);
     const { price, title, qty } = this.props.product;
+    const {
+      product,
+      onIncreaseQuantity,
+      onDecreaseQuantity,
+      onDeleteProduct
+    } = this.props;
     return (
       <div className="cart-item">
         {this.props.jsx}
@@ -20,18 +26,19 @@ class CartItem extends React.Component {
               alt="increase"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
-              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+              onClick={() => onIncreaseQuantity(product)}
             />
             <img
               alt="decrease"
               className="action-icons"
               src="https://cdn-icons-png.flaticon.com/128/992/992683.png"
-              onClick={this.decreaseQuantity}
+              onClick={() => onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
               className="action-icons"
               src="https://t4.ftcdn.net/jpg/01/90/89/15/240_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+              onClick={() => onDeleteProduct(product.id)}
             />
           </div>
         </div>
