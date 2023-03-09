d21444e6edb204343814258c0118af2d57a1be62
Unmesh Pawar <unplfc@gmail.com>
2023-03-09 18:12:31 +0530

    olol

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from  'firebase/compat/app';
+import 'firebase/compat/firestore';
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyArL47LzD-oRCg8g9o8EO3xaN1bb68CWrk",
@@ -17,7 +18,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
