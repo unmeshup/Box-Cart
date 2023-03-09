401e29913ae04a25b43c58ff3112b845928f96f6
Unmesh Pawar <unplfc@gmail.com>
2023-03-09 21:11:45 +0530

    one

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@ import './index.css';
 import App from './App';
 import firebase from  'firebase/compat/app';
 import 'firebase/compat/firestore';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyArL47LzD-oRCg8g9o8EO3xaN1bb68CWrk",
+    authDomain: "box-cart-20b15.firebaseapp.com",
+    projectId: "box-cart-20b15",
+    storageBucket: "box-cart-20b15.appspot.com",
+    messagingSenderId: "68891610804",
+    appId: "1:68891610804:web:59a9b488469fa0cf6fd1e1"
+  };
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyArL47LzD-oRCg8g9o8EO3xaN1bb68CWrk",
-  authDomain: "box-cart-20b15.firebaseapp.com",
-  projectId: "box-cart-20b15",
-  storageBucket: "box-cart-20b15.appspot.com",
-  messagingSenderId: "68891610804",
-  appId: "1:68891610804:web:59a9b488469fa0cf6fd1e1"
-};
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
